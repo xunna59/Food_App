6e547eb5966ddba1f2fec66fb1f46bbc80ec9516
xunna <nnaemekauzuegbu59@gmail.com>
2024-10-29 16:00:45 +0100

    updates ssl fix for postgres

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -5,7 +5,13 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 
